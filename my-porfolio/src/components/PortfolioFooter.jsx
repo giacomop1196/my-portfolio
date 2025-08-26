9569d88d8c19d6633a4433673e40a76b118937fa
giacomop1196 <giacomo.pillitteri65@gmail.com>
2025-08-26 10:00:23 +0200

    footer text

--- my-porfolio/src/components/PortfolioFooter.jsx
+++ my-porfolio/src/components/PortfolioFooter.jsx
@@ -4,6 +4,7 @@ const PortfolioFooter = () => {
     return (
         <footer className="footer-custom">
             <Container className="text-center footer-content">
+                <p className='mb-2'>React - React Bootstrap - React Router</p>
                 <p>&copy; 2025 Giacomo Pillitteri. Tutti i diritti riservati.</p>
             </Container>
         </footer>
