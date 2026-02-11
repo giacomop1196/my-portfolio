35e73e6b713384056ea35185909d3ac7f6c8338d
giacomop1196 <giacomo.pillitteri65@gmail.com>
2026-02-11 10:29:23 +0100

    commit

--- my-porfolio/src/components/PortfolioFooter.jsx
+++ my-porfolio/src/components/PortfolioFooter.jsx
@@ -5,10 +5,21 @@ const PortfolioFooter = () => {
         <footer className="footer-custom">
             <Container className="text-center footer-content">
                 <p className='mb-2'>React - React Bootstrap - React Router</p>
+
+                
+                {/* Dati Fiscali e Contatti Legali */}
+                <div className="small mb-2">
+                    <p className="mb-0">P.IVA: 07390840820</p>
+                    <p className="mb-0">
+                        PEC: <a href="mailto:giacomopillitteri" className="text-decoration-none text-white">giacomopillitteri@pec.it</a>
+                    </p>
+                </div>
+
+                {/* Info Principali */}
                 <p>&copy; 2025 Giacomo Pillitteri. Tutti i diritti riservati.</p>
+
             </Container>
         </footer>
     );
 };
-
 export default PortfolioFooter;
